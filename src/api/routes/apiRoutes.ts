16e65c382a070a2ba860111d4a08ef5248d30191
duyenlan151 <1660144.16ck1@gmail.com>
2025-03-13 17:17:47 +0700

    feat: add route

--- src/api/routes/apiRoutes.ts
+++ src/api/routes/apiRoutes.ts
@@ -3,6 +3,7 @@ import { getMetadataAndRaceData } from '@/api/controllers/f1Controller';
 
 const router = express.Router();
 
+router.get('/', (_, res) => res.json({ message: 'Home page' }));
 router.get('/crawl', (_, res) => res.json({ message: 'Crawl initiated via WebSocket' }));
 router.get('/metadata', getMetadataAndRaceData);
 
