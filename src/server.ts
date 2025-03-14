274c68e06fb7087438ea163b376eb647d882c92a
duyenlan151 <1660144.16ck1@gmail.com>
2025-03-14 18:13:09 +0700

    feat: revert code

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import { env } from './config/env';
 import routes from './api/routes/apiRoutes';
-// import './websocket/server';
+import './websocket/server';
 import { logger } from './utils/logger';
 
 const app = express();
 
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
-// app.use('/', routes);
-app.get('/', (_, res) => res.json({ message: 'Home page' }));
+app.use('/', routes);
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
   logger.error(`Unhandled error: ${err.message}`);
